773df1a4ba8f29c06035cb6afecaba5aa1866f04
mibi88 <76903855+mibi88@users.noreply.github.com>
2021-06-20 12:38:36 +0200

    v.0.4

--- index.tcl
+++ index.tcl
@@ -1,7 +1,7 @@
 #!/usr/bin/wish
 
-package require Tcl 8.5
-package require Tk 8.5
+package require Tcl 8.6
+package require Tk 8.6
 package require ctext
 
 set tclfile_path [file dirname [file normalize [info script]]]
@@ -9,13 +9,23 @@ set fontconfig_path "$tclfile_path/font.mibiconfig"
 puts $fontconfig_path
 set font_o [open $fontconfig_path r]
 set font_data [read $font_o]
+close $font_o
 set font_data_list [split $font_data "\n"]
-set font_familly [lindex $font_data_list 0]
+set font_family [lindex $font_data_list 0]
 set font_size [lindex $font_data_list 1]
-set tabswidth 4
+set file_tabswidth [lindex $font_data_list 2]
+set tabswidth "{ $file_tabswidth c numeric 1c }"
+set cmdconfig_path "$tclfile_path/tclconf.mibiconfig"
+set cmd_o [open $cmdconfig_path r]
+set cmd_data [read $cmd_o]
+set cmd_data_list [split $cmd_data "\n"]
+set cmd [lindex $cmd_data_list 0]
+set tclsh_cmd [lindex $cmd_data_list 1]
+close $cmd_o
 set chan "none"
 set filename "None"
 set saved 1
+set index 1.0
 proc askabort {  } {
   global saved
   if { $saved == 0 } {
@@ -49,10 +59,10 @@ proc open_f {  } {
   global filename
   global saved
   set filetypes {
-    {{Text Files}       {.txt}        }
     {{TCL Scripts}      {.tcl}        }
     {{Python Scripts}   {.py}         }
     {{Python Scripts}   {.pyw}        }
+    {{Text Files}       {.txt}        }
     {{All Files}        *             }
   }
   if { [askabort] == true} {
@@ -88,10 +98,10 @@ proc saveas_f {  } {
   puts "Save as"
   global filename
   set filetypes {
-    {{Text Files}       {.txt}        }
     {{TCL Scripts}      {.tcl}        }
     {{Python Scripts}   {.py}         }
     {{Python Scripts}   {.pyw}        }
+    {{Text Files}       {.txt}        }
     {{All Files}        *             }
   }
   set filename [tk_getSaveFile -filetypes $filetypes -confirmoverwrite true]
@@ -127,7 +137,12 @@ proc quit_w {  } {
   }
 }
 proc about_w {  } {
-  set textvar "Mibi's Tcl Editor v.0.3\nby mibi88\nLicense : GNU GPL v2 or later\nCodename : v13\n2021-2021"
+  global tcl_platform
+  set tclv [info patchlevel]
+  set os_t $tcl_platform(os)
+  set osv $tcl_platform(osVersion)
+  set processor $tcl_platform(machine)
+  set textvar "Mibi's Tcl Editor v.0.4\nby mibi88\nLicense : GNU GPL v2 or later\nCodename : v14\n2021-2021\n\nTcl version : $tclv\nOperating system : $os_t $osv\nProcessor : $processor"
   toplevel .about
   wm transient .about .
   text .about.info
@@ -136,18 +151,45 @@ proc about_w {  } {
    pack .about.info -fill both -expand true
 }
 proc settings_w {  } {
-  global font_familly
+  global font_family
   global font_size
   global fontconfig_path
+  global tabswidth
+  global file_tabswidth
+  global cmd
+  global tclsh_cmd
+  global .pan.mainf.textf
   toplevel .settings
   wm transient .settings .
+  wm title .settings "Settings"
+  label .settings.font_i -text "Font family :"
   ttk::combobox .settings.font -values [font families] -state normal
-  .settings.font set $font_familly
-  spinbox .settings.fontsize -from 9 -to 96
+  .settings.font set $font_family
+  label .settings.fontsize_i -text "Font size :"
+  spinbox .settings.fontsize -from 9 -to 96 -state readonly
   .settings.fontsize set $font_size
-  button .settings.apply -text "Apply" -command { set font_size [.settings.fontsize get]; set font_familly [.settings.font get]; set font_o [open $fontconfig_path w]; puts $font_o $font_familly; puts $font_o $font_size; close $font_o; font configure textboxfont -family $font_familly -size $font_size}
+  label .settings.tabw_i -text "Tab width (c) :"
+  spinbox .settings.tabw -from 1 -to 12 -state readonly
+  .settings.tabw set $file_tabswidth
+  label .settings.cmd_i -text "Run command\n(\"<f>\" will be\nreplaced with the\nname of the file\nto run) :"
+  entry .settings.cmd
+  label .settings.cmd_tclsh_i -text "Console command :"
+  entry .settings.cmd_tclsh
+  .settings.cmd delete 0 end
+  .settings.cmd insert 0 $cmd
+  .settings.cmd_tclsh delete 0 end
+  .settings.cmd_tclsh insert 0 $tclsh_cmd
+  button .settings.apply -text "Apply" -command { set font_size [.settings.fontsize get]; set font_family [.settings.font get]; set file_tabswidth [.settings.tabw get]; set tabswidth "{ $file_tabswidth c numeric 1c }"; set cmd [.settings.cmd get]; set tclsh_cmd [.settings.cmd_tclsh get]; set font_o [open $fontconfig_path w]; puts $font_o $font_family; puts $font_o $font_size; puts $font_o $file_tabswidth; close $font_o; font configure textboxfont -family $font_family -size $font_size; .pan.mainf.textf.st configure -tabs $tabswidth; set cmd_o [open $cmdconfig_path w]; puts $cmd_o $cmd; puts $cmd_o $tclsh_cmd; close $cmd_o }
+  pack .settings.font_i
   pack .settings.font
+  pack .settings.fontsize_i
   pack .settings.fontsize
+  pack .settings.tabw_i
+  pack .settings.tabw
+  pack .settings.cmd_i
+  pack .settings.cmd
+  pack .settings.cmd_tclsh_i
+  pack .settings.cmd_tclsh
   pack .settings.apply
 }
 proc runinfo {  } {
@@ -162,13 +204,14 @@ proc run_f {  } {
   global filename
   global tclfile_path
   global chan
+  global cmd
   .pan.outf.out configure -state normal
   .pan.outf.out insert end [runinfo]
   .pan.outf.out tag configure errort -foreground red
   .pan.outf.out configure -state disabled
     if { $saved == 1 } {
       if { $filename != "None" } {
-        set cmdstr "wish $filename"
+        set cmdstr [regsub "<f>" $cmd $filename]
         set command $cmdstr
         proc receive {chan} {
           .pan.outf.out configure -state normal
@@ -195,11 +238,69 @@ proc run_f {  } {
   }
 }
 proc consoleshow_w {  } {
-  set console [open |[concat "wish tkcon.tcl" 2>@1]]
+  global cmd
+  global tclfile_path
+  puts [regsub "<f>" $cmd "tkcon.tcl"]
+  set console [open |[regsub "<f>" $cmd "$tclfile_path/tkcon.tcl"]]
 }
-#proc consolehide_w {  } {
-#    close |$console
-#}
+proc replace {  } {
+  global .pan.mainf.textf.st
+  toplevel .replace
+  wm transient .replace .
+  wm title .replace "Replace"
+  label .replace.orgstr_i -text "To replace :"
+  entry .replace.orgstr
+  label .replace.newstr_i -text "With :"
+  entry .replace.newstr
+  button .replace.replaceb -text "Replace next" -command { set org [.replace.orgstr get]; set new [.replace.newstr get]; set text [.pan.mainf.textf.st get 1.0 end]; set text_a [regsub $org $text $new]; .pan.mainf.textf.st delete 1.0 end; .pan.mainf.textf.st insert end $text_a }
+  pack .replace.orgstr_i
+  pack .replace.orgstr
+  pack .replace.newstr_i
+  pack .replace.newstr
+  pack .replace.replaceb
+}
+proc search {  } {
+  global .pan.mainf.textf.st
+  global index
+  set index 1.0
+  toplevel .search
+  wm transient .search .
+  wm title .search "Search"
+  label .search.str_i -text "To search :"
+  entry .search.str
+  button .search.searchb -text "Find next\n(click more than\none time to highlight\nalso the next word)" -command { puts $index; set str [.search.str get]; set position [.pan.mainf.textf.st  search -count n -- $str $index+2c]; set index $position+${n}c; .pan.mainf.textf.st tag add thing $position $position+${n}c; .pan.mainf.textf.st tag configure thing -background yellow }
+  pack .search.str_i
+  pack .search.str
+  pack .search.searchb
+}
+proc removesearchhighlight {  } {
+  .pan.mainf.textf.st tag remove thing 1.0 end
+}
+proc copy_t {  } {
+  clipboard clear
+  selection own
+  set text [selection get]
+  clipboard append $text
+}
+proc cut_t {  } {
+  clipboard clear
+  set sel [selection own]
+  set text [selection get]
+  clipboard append $text
+  catch {
+    $sel delete sel.first sel.last
+  }
+}
+proc paste_t {  } {
+  if {[catch {clipboard get} contents]} {
+    puts "error"
+  } else {
+    selection own
+    set text [clipboard get]
+    set position [.pan.mainf.textf.st index insert]
+    .pan.mainf.textf.st insert $position [clipboard get]
+  }
+}
 settitle
 panedwindow .pan -orient vertical -showhandle 1 -sashwidth 3 -sashrelief groove
 menu .mb
@@ -214,13 +315,24 @@ menu .mb.file -tearoff 0
 .mb.file add separator
 .mb.file add command -label "Quit          Ctrl-Q" -command { quit_w }
 .mb add cascade -label "File" -menu .mb.file
+menu .mb.edit -tearoff 0
+.mb.edit add command -label "Copy            Ctrl-C" -command { copy_t }
+.mb.edit add command -label "Cut               Ctrl-X" -command { cut_t }
+.mb.edit add command -label "Paste            Ctrl-V" -command { paste_t }
+.mb.edit add separator
+.mb.edit add command -label "Search ...     Ctrl-F" -command { search }
+.mb.edit add command -label "Replace ...   Ctrl-H" -command { replace }
+.mb.edit add command -label "Remove search tags ..." -command { removesearchhighlight }
+.mb.edit add separator
+.mb.edit add command -label "Undo            Ctrl-Z" -command { .pan.mainf.textf.st edit undo }
+.mb.edit add command -label "Redo            Ctrl-Y" -command { .pan.mainf.textf.st edit redo }
+.mb add cascade -label "Edit" -menu .mb.edit
 menu .mb.tools -tearoff 0
 .mb.tools add command -label "Settings" -command { settings_w }
-.mb.file add separator
+.mb.tools add separator
 .mb.tools add command -label "Run           F5" -command { run_f }
-.mb.file add separator
+.mb.tools add separator
 .mb.tools add command -label "Show TkCon" -command { consoleshow_w }
-#.mb.tools add command -label "Hide TkCon" -command { consolehide_w }
 .mb add cascade -label "Tools" -menu .mb.tools
 menu .mb.about -tearoff 0
 .mb.about add command -label "About" -command { about_w }
@@ -230,22 +342,25 @@ frame .pan.mainf
 frame .pan.mainf.textf
 scrollbar .pan.mainf.scroll -command {.pan.mainf.textf.st yview} -orient vertical -width 8
 scrollbar .pan.mainf.textf.scrollh -command {.pan.mainf.textf.st xview} -orient horizontal  -width 8
-font create textboxfont -family $font_familly -size $font_size
+font create textboxfont -family $font_family -size $font_size
 ctext .pan.mainf.textf.st -yscrollcommand {.pan.mainf.scroll set} -xscrollcommand {.pan.mainf.textf.scrollh set} -wrap none -tabs $tabswidth -undo true -font textboxfont
 pack .pan.mainf.textf -fill both -expand yes -side left
 pack .pan.mainf.textf.st -fill both -expand yes
 pack .pan.mainf.scroll -fill y -side left
 pack .pan.mainf.textf.scrollh -fill x
 # syntax hightlighting
-::ctext::addHighlightClass .pan.mainf.textf.st tclkeywords blue [list set info interp uplevel global upvar if elseif else proc text button label entry package puts toplevel frame canvas return for while switch case lappend unset variable foreach namespace for incr list regsub close expr foreach llengthappend concat format load return array gets lrange proc switchfile glob lappend lreplace putsbreak global lsearch set catch eval lindex lsort while exec if linsert open regexp source clock exit package split unknown after info pid rename string unset fblocked interp pkg_mkIndex subst update continue fconfigure join scan uplevel bgerror eof seek tclvars upvar error fileevent library pwd tell vwait filename history read socket time cd flush trace]
+::ctext::addHighlightClass .pan.mainf.textf.st tclkeywords blue [list set info interp uplevel global upvar if elseif else proc text button label entry package puts toplevel frame canvas return for while switch case lappend unset variable foreach namespace for incr list regsub close expr foreach llengthappend concat format load return array gets lrange proc switchfile glob lappend lreplace putsbreak global lsearch set catch eval lindex lsort while exec if linsert open regexp source clock exit package split unknown after info pid rename string unset fblocked interp pkg_mkIndex subst update continue fconfigure join scan uplevel bgerror eof seek tclvars upvar error fileevent library pwd tell vwait filename history read socket time cd flush trace pack insert delete get grid place search]
 ::ctext::addHighlightClass .pan.mainf.textf.st modulenames purple [list Tk TclOO Tcl ctext autoscroll canvas chatwidget crosshair cursor datefield Diagrams getstring tklib_history ico ipentry khim ntext plotchart style swaplist tablelist tkpiechart tooltip widget]
 ::ctext::addHighlightClass .pan.mainf.textf.st kwglobal red [list require provide tearoff]
 ::ctext::addHighlightClassWithOnlyCharStart .pan.mainf.textf.st args brown \-
 ::ctext::addHighlightClassWithOnlyCharStart .pan.mainf.textf.st vars green \$
 ::ctext::addHighlightClassForRegexp .pan.mainf.textf.st comments gray {#[^\n\r]*}
 ::ctext::addHighlightClassForSpecialChars .pan.mainf.textf.st brackets darkblue {[]{}}
-::ctext::addHighlightClassForSpecialChars .pan.mainf.textf.st quotes darkgreen {"'}
+::ctext::addHighlightClassForRegexp .pan.mainf.textf.st aquotes darkgreen {"(\\"|[^"])*"}
+::ctext::addHighlightClassForRegexp .pan.mainf.textf.st bquotes darkgreen {'(\\'|[^'])*'}
 ::ctext::addHighlightClassForSpecialChars .pan.mainf.textf.st math darkred {+=*-/&^%!|<> 0 1 2 3 4 5 6 7 8 9 .}
+::ctext::addHighlightClass .pan.mainf.textf.st bools darkred [list true false]
+::ctext::addHighlightClassForRegexp .pan.mainf.textf.st class darkblue {::}
 frame .pan.outf
 text .pan.outf.out -wrap word  -tabs $tabswidth -state disabled -yscrollcommand { .pan.outf.outscroll set }
 scrollbar .pan.outf.outscroll -orient vertical -command { .pan.outf.out yview } -width 8
@@ -269,5 +384,7 @@ bind . <Control-o> {  open_f  }
 bind . <Control-n> {  new_f  }
 bind . <Control-q> {  quit_w  }
 bind . <F5> {  run_f  }
+bind . <Control-f> {  search  }
+bind . <Control-h> {  replace  }
 
 wm protocol . WM_DELETE_WINDOW { quit_w }
