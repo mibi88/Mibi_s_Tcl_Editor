9c2a4e9f415025be87c10c75fe8ad3fbc8c240ed
mibi88 <76903855+mibi88@users.noreply.github.com>
2021-12-03 18:42:49 +0100

    Better syntax highlighting.

---
+++ langs/highlight.tcl
@@ -0,0 +1,10 @@
+::ctext::addHighlightClass .pan.mainf.textf.st pykeywords blue [list abs dict help min setattr all dir hex next slice any divmod id object sorted ascii enumerate input oct staticmethod bin eval int open str bool exec isinstance ord sum bytearray filter issubclass pow super bytes float iter print tuple callable format len property type chr frozenset list range vars classmethod getattr locals repr zip compile globals map reversed __import__ complex hasattr max round delattr hash memoryview set if else elif def while for]
+::ctext::addHighlightClass .pan.mainf.textf.st modulenames purple [list tkinter]
+::ctext::addHighlightClassForSpecialChars .pan.mainf.textf.st brackets darkblue {()}
+::ctext::addHighlightClassForSpecialChars .pan.mainf.textf.st math darkred {+=*-/&^%!|<> 0 1 2 3 4 5 6 7 8 9 .}
+::ctext::addHighlightClass .pan.mainf.textf.st bools darkred [list True False]
+::ctext::addHighlightClassForRegexp .pan.mainf.textf.st aquotes darkgreen {"(\\"|[^"])*"}
+::ctext::addHighlightClassForRegexp .pan.mainf.textf.st bquotes darkgreen {'(\\'|[^'])*'}
+::ctext::addHighlightClassForRegexp .pan.mainf.textf.st comments gray {#[^\n\r]*}
+
+
